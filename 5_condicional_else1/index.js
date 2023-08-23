e8b5760dbdec0aae5964a6c3962e46d53ca56173
Lariany <110117446+LarianySales@users.noreply.github.com>
2023-08-23 16:17:27 -0300

    Add files via upload

---
+++ 5_condicional_else1/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 5004
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs.engine())
+app.set('view engine','handlebars')
+
+app.get('/dashboard',(req,res)=>{
+   return res.render('dashboard')
+})
+
+app.get('/',(req,res)=>{
+    const user = {
+      name: 'Cátia',
+     surname: 'Elena',
+     age: 30
+    }
+    const palavra = 'teste'
+    const auth = true
+    const approved = true
+    return res.render('home', {user:user, palavra,auth, approved}) 
+    //agora a home tem acesso as info do obj 
+      
+   })
+   app.listen(port,()=>{
+       console.log(`Servidor on ${port}🦦`)
+   })
